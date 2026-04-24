4858b8942d33389878976dffca3bcf01abce257d
flemming-cramer <168538493+flemming-cramer@users.noreply.github.com>
2026-04-24 11:33:58 +0200

    Updated chatcore.py

--- chatcore.py
+++ chatcore.py
@@ -1,5 +1,5 @@
 from langchain_community.llms import Ollama
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
